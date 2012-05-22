43f60fbd026d01683e66cafb2cc31c3c3545906f
Nathan Myers <nathanemyers@gmail.com>
2012-05-22 11:55:21 -0500

    inital commit

---
+++ masscommand.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import argparse, re, os
+
+parser = argparse.ArgumentParser(description='runs a command on each box')
+parser.add_argument('command', action="store")
+parser.add_argument('host', nargs='*')
+
+args = parser.parse_args()
+hosts = []
+if args.host:
+	hosts = args.host
+else:
+	sshconfig = open('/home/nm021421/.ssh/config', 'r')
+	pattern = re.compile('^host=([a-z].*)')
+	for line in sshconfig:
+		host = pattern.match(line)
+		if host:
+			hosts.append(host.group(1))
+
+for host in hosts:
+	print '=== ' + host + ' ==='
+	os.system('ssh ' + host + " '" + args.command + "'")
---
+++ propagatefile.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+
+use strict;
+use sigtrap 'handler' => \&killtrap, 'INT';
+
+# author - nm021421
+# JUN/2011
+
+# this tool scp's a file to each non-capitalized host in your .ssh/config file
+# I find that it's a pretty handy thing to have around
+
+open( CONFIGFILE, "/home/nm021421/.ssh/config" )
+  or die "Unable to open ssh config file!";
+my $num_args = $#ARGV + 1;
+if ( $num_args < 1 ) {
+    print "USAGE: ./propagate <file> [<destination location>]\n";
+    exit;
+}
+my $line;
+my $file     = $ARGV[0];
+my $location = "~/";
+if ( $num_args == 2 ) {
+    $location = $ARGV[1];
+}
+print "Propagating [$file -> $location]";
+
+my @hosts;
+while ( $line = <CONFIGFILE> ) {
+    if ( $line =~ m/^host=([^A-Z].*)/ ) {
+        push( @hosts, $1 );
+    }
+}
+my $sentcounter = 0;
+for my $host (@hosts) {
+    print ".";
+    if ( -d $file ) {
+        `scp -r $file $host:$location`;
+    }
+    else {
+        `scp $file $host:$location`;
+    }
+    $sentcounter++;
+}
+print "\n";
+
+sub killtrap() {
+    print "\n** Process Aborted! **\n";
+    for my $host (@hosts) {
+        if ( $sentcounter > 0 ) {
+            print "Sent: $host\n";
+        }
+        else {
+            print "Failed: $host\n";
+        }
+        $sentcounter--;
+    }
+    exit(1);
+}
